e08d8e6fe020dd73898a7f29df3b690a5150ced2
khebrati <khebrati@gmail.com>
2023-04-21 21:22:42 +0430

    made 4 classes, initial code.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace tic_tac_toe
+﻿using static System.Console;
+using static System.Convert;
+namespace tic_tac_toe
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    internal class PlayGround {
+        static void main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WriteLine("welcome!");
+            WriteLine("what do you want to do?");
+            WriteLine("1.start a new game");
+            WriteLine();
+            if (ToInt32(ReadLine()) == 1){
+                Game game = new();
+            }
+        }
+    }
+    internal class Player
+    {
+        
+    }
+    internal class Bot : Player { }
+    internal class User : Player { }
+    internal class Game
+    {
+        private char[,] board;
+        private User user;
+        private Bot bot;
+        public Game(char[,] board)
+        {
+            this.board = board;
+        }
+        public Game() {
+            this.board = new char[3,3] ;
         }
     }
 }
