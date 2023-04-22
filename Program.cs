06049b6dbab4aa4c561389ae21bb9dd6aa89d0dd
khebrati <khebrati@gmail.com>
2023-04-22 10:28:02 +0430

    can print an empty board.

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace tic_tac_toe
             WriteLine("welcome!");
             WriteLine("what do you want to do?");
             WriteLine("1.start a new game");
-            WriteLine();
             if (ToInt32(ReadLine()) == 1){
                 Game game = new();
+                WriteLine(game.Board.ToString());
             }
         }
     }
@@ -21,16 +21,41 @@ namespace tic_tac_toe
     internal class Bot : Player { }
     internal class User : Player { }
     internal class Board {
-        public char[,] elements { get; set; }
-        public char this[char c0,char c1] { get { return elements[c0,c1]; } set { elements[c0,c1] = value; } }
-        
+        public char[,] Elements { get; set; }
+        public Board()
+        {
+            Elements = new char[3,3];
+            for(int i = 0 ; i < 3; i++)
+            {
+                for(int j  =0; j < 3; j++)
+                {
+                    Elements[i, j] = ' ';
+                }
+            }
+        }
+        public char this[int i,int j] { get { return Elements[i,j]; } set { Elements[i,j] = value; } }
+        public override string ToString()
+        {
+            return $" --- --- --- \n" +
+                   $"| {Elements[0, 0]} | {Elements[0, 1]} | {Elements[0, 2]} |\n" +
+                   $"|---|---|---|\n" +
+                   $"| {Elements[1, 0]} | {Elements[1, 1]} | {Elements[1, 2]} |\n" +
+                   $"|---|---|---|\n" +
+                   $"| {Elements[2, 0]} | {Elements[2, 1]} | {Elements[2, 2]} |\n" +
+                   $" --- --- --- \n";
+        }
     }
     internal class Game
     {
-        public Board board { get; set; }
-        public User user { get; set; }
-        public Bot bot { get; set; }
-        
+        public Board Board { get; set; }
+        public User User { get; set; }
+        public Bot Bot { get; set; }
+        public Game()
+        {
+            Board = new();
+            User = new();
+            Bot = new();
+        }
         
     }
 }
